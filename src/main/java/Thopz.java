14d27bb621094c8074bcf59eb4c97b96eb1309b7
shockingly134 <shananth.sivakumar@gmail.com>
2025-08-22 11:41:59 +0800

    Level 2 (Updated)

--- src/main/java/Thopz.java
+++ src/main/java/Thopz.java
@@ -1,3 +1,6 @@
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Thopz {
@@ -8,17 +11,29 @@ public class Thopz {
             "How may I help you?" ;
     public static String goodbye = "Chaoz. See you ! ";
 
-    // Repetition of words for chatbot
+    // Adding words to list, used Arraylist functions to achieve this
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
+        List<String> ls = new ArrayList<>();
 
         System.out.println("" + greetings);
         String repeat = scan.next();
 
+
         while (!repeat.equals("bye")) {
-            System.out.println("" + repeat);
+            if(repeat.equals("list")) {
+                for(int i= 0; i< ls.size(); i++) {
+                    System.out.println((i+1) + ". " + ls.get(i));
+                }
+                }
+            else {
+                ls.add(repeat);
+                System.out.println("added: " + repeat);
+            }
+
             repeat = scan.next();
+
         }
 
         System.out.println("" + goodbye);
