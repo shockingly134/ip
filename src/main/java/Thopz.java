2bd120f0a10bac2f1edef145ba19542578f9ccef
shockingly134 <shananth.sivakumar@gmail.com>
2025-08-23 17:04:42 +0800

    LEVEL 6

--- src/main/java/Thopz.java
+++ src/main/java/Thopz.java
@@ -24,65 +24,106 @@ public class Thopz {
 
 
         while (!inp.equals("bye")) {
-            if(inp.equals("list")) {
-                for(int i= 0; i< ls.size(); i++) {
-                    System.out.println((i+1) + ". " + ls.get(i));
+            try {
+                if (inp.equals("list")) {
+                    for (int i = 0; i < ls.size(); i++) {
+                        System.out.println((i + 1) + ". " + ls.get(i));
+                    }
+                } else if (inp.startsWith("mark ")) {
+                    try {
+                        String[] parts = inp.split(" ");
+                        int no = Integer.parseInt(parts[1]);
+                        if (no >= 1 && no <= ls.size()) {
+                            ls.get(no - 1).markTask();
+                            System.out.println("Solidd! I have marked this task as done \n" + ls.get(no - 1).toString());
+                        } else {
+                            System.out.println("bruh, out of range");
+                        }
+                    } catch (NumberFormatException e) {
+                        System.out.println("no valid number");
+                    }
                 }
+                // Deadline would have one /, so splitting words and taking the required once
+                else if (inp.startsWith("deadline ")) {
+                    String des =inp.substring(9);
+                    if(des.isEmpty()) {
+                        throw new IllegalArgumentException("You should give an description!");
+                    }
+                    else if (!inp.contains("/")) {
+                        throw new IllegalArgumentException("You should add a / after your description to specify deadline");
+                    }
+
+                    String[] parts = inp.split("/");
+                    String desc = parts[0].substring(9);
+                    String due = parts[1];
+                    ls.add(new Deadline(desc, due));
+                    System.out.println("Orrite, I have added " + inp + "\nYou have " + ls.size() + " tasks");
                 }
 
-            else if (inp.startsWith("mark")) {
-               try {
-                   String[] parts = inp.split(" ");
-                   int no = Integer.parseInt(parts[1]);
-                   if(no>=1 && no<= ls.size()) {
-                       ls.get(no-1).markTask();
-                       System.out.println("Solidd! I have marked this task as done \n"  + ls.get(no-1).toString());
-                   }
-                   else {
-                       System.out.println("bruh, out of range");
-                   }
-               }
-               catch (NumberFormatException e) {
-                   System.out.println("no valid number");
-               }
+
+                // Event would have // which show start and end, so by splitting can create new Event task
+                else if (inp.startsWith("event ")) {
+                    String des =inp.substring(9);
+                    if(des.isEmpty()) {
+                        throw new IllegalArgumentException("You should give an description!");
+                    }
+                    else if (!inp.contains("/")) {
+                        throw new IllegalArgumentException("You should add 2 slashes / after your description " +
+                                "to specify event start and end date e.g(eating / Monday / Tuesday");
+                    }
+                    String[] parts = inp.split("/");
+                    String desc = parts[0].substring(6);
+                    String begin = parts[1];
+                    String end = parts[2];
+                    ls.add(new Events(desc, begin, end));
+                    System.out.println("Orrite, I have added " + inp + "\nYou have " + ls.size() + " tasks");
+                }
+
+                // Todo task with js a message
+
+                else if (inp.startsWith("todo ")) {
+                    String des =inp.substring(5);
+                    if(des.isEmpty()) {
+                        throw new IllegalArgumentException("You should give an description!");
+                    }
+                    String desc = inp.substring(5);
+                    ls.add(new Todo(desc));
+                    System.out.println("Orrite, I have added " + inp + "\nYou have " + ls.size() + " tasks");
+                }
+
+                else if (inp.startsWith("delete ")) {
+                    try {
+                        String[] parts = inp.split(" ");
+                        int no = Integer.parseInt(parts[1]);
+                        if (no >= 1 && no <= ls.size()) {
+                            ls.remove(no-1);
+                            System.out.println("Solidd! I have deleted this task as done \n" + ls.get(no - 1).toString());
+                        } else {
+                            System.out.println("bruh, out of range");
+                        }
+                    } catch (NumberFormatException e) {
+                        System.out.println("no valid number given");
+                    }
+
+                }
+                else {
+                    throw new IllegalArgumentException("uhh, idk what you are saying! You gotta start with what kind" +
+                            "of task you wish to add,(e.g todo homework, deadline eating/Monday, etc");
+                }
+
             }
-            // Deadline would have one /, so splitting words and taking the required once
-            else if (inp.startsWith("deadline")) {
-                String[] parts = inp.split("/");
-                String desc = parts[0].substring(9);
-                String due = parts[1];
-                ls.add(new Deadline(desc, due));
-                System.out.println("Orrite, I have added " + inp + "\nYou have " +ls.size() + " tasks");
-            }
 
+            catch(IllegalArgumentException e) {
 
-            // Event would have // which show start and end, so by splitting can create new Event task
-            else if (inp.startsWith("event")) {
-                String[] parts = inp.split("/");
-                String desc = parts[0].substring(6);
-                String begin = parts[1];
-                String end = parts[2];
-                ls.add(new Events(desc, begin, end));
-                System.out.println("Orrite, I have added " + inp + "\nYou have " +ls.size() + " tasks");
-            }
+                System.out.println("OOPS!!!!!" +e.getMessage());
 
-            // Todo task with js a message
-
-            else if (inp.startsWith("todo")) {
-                String desc = inp.substring(5);
-                ls.add(new Todo(desc));
-                System.out.println("Orrite, I have added " + inp +  "\nYou have " +ls.size() + " tasks");
-            }
-
-
-            else {
-                ls.add(new Task(inp));
-                System.out.println("added: " + inp);
             }
 
             inp = scan.nextLine();
 
         }
+
+
 
         System.out.println("" + goodbye);
 
